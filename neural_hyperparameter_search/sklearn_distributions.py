d24b4283035d9f927e5c55764b378f34b41ecf3d
Matthew McDermott <mmd@mit.edu>
2018-08-13 21:22:09 -0400

    Added more sklearn distributions.

--- neural_hyperparameter_search/sklearn_distributions.py
+++ neural_hyperparameter_search/sklearn_distributions.py
@@ -2,6 +2,42 @@ import scipy.stats as ss
 
 from .distributions import *
 
+#TODO(mmd): Finish
+#SGD_dist = DictDistribution({
+#    'loss': ['hinge', 'log', 'modified_huber', 'squared_hinge', 'perceptron'
+#    'penalty': ['l1', 'l2', 'none', 'elasticnet'],
+#    'alpha': ss.uniform(1e-6, 1.5),
+#    'l1_ratio': ss.beta(5, 5),
+#    'class_weight': [None, 'balanced'],
+#    'C': ss.uniform(1e-4, 10),
+#})
+
+Lasso_dist = DictDistribution({'alpha': ss.uniform(1e-4, 10)})
+RF_reg_dist = DictDistribution({
+    'n_estimators': [ss.poisson(10), ss.poisson(50), ss.poisson(100), ss.poisson(250), ss.poisson(500)],
+    'criterion': ['mse', 'mae'],
+    'max_depth': ss.randint(2, 10),
+    'min_samples_split': ss.randint(2, 75),
+    'min_samples_leaf': ss.randint(1, 50),
+})
+MLP_reg_dist = DictDistribution({
+    'hidden_layer_sizes': ListRV(
+        el_rv=[ss.poisson(25), ss.poisson(50), ss.poisson(250)],
+    ),
+    'activation': ['logistic', 'tanh', 'relu'],
+    'solver': ['lbfgs', 'sgd', 'adam'],
+    'alpha': ss.uniform(1e-6, 1.5),
+    'batch_size': ['auto', ss.randint(16, 512)],
+    'learning_rate': ['constant', 'invscaling', 'adaptive'],
+    'learning_rate_init': ss.uniform(1e-5, 1),
+    'power_t': ss.beta(5, 5),
+    'max_iter': ss.randint(50, 700),
+    'momentum': ss.beta(9, 1),
+    'early_stopping': [True, False], # Coin(p) gives weighted
+    'validation_fraction': ss.beta(1, 9),
+})
+
+
 LR_dist = DictDistribution({
     'penalty': ['l1', 'l2'],
     'class_weight': [None, 'balanced'],
